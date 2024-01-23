4e6b0a6464df852cf23bf7086dcaa19b9e452e45
Stefan Mehner <stefan.mehner@b-tu.de>
2024-01-23 10:07:19 +0100

    update

--- traffic_generator/generate_day_traffic.sh
+++ traffic_generator/generate_day_traffic.sh
@@ -8,5 +8,5 @@ PYTHON=${CONDA}/envs/mini-ipd/bin/python3
 
 
 screen -d -m -S netflow_collector bash -c "$PYTHON ../netflow_collector/netflow_collector.py -conda $CONDA -v"
-$PYTHON traffic_controller.py --start -w 60 -f 5000 --killcollector --conda $CONDA
+$PYTHON traffic_controller.py --start -w 60 -f 5000 --killcollector --conda $CONDA -g
 $PYTHON ../ipd-implementation/tools/connect_netflow.py
